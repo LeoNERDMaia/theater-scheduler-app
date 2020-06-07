50f0916879943035e3e0445a97ca51e976c8d6d3
Leonardo Geyer Maia <leogmaia@gmail.com>
2020-06-07 10:31:53 -0300

    First commit for the base project
    This is the first commit on the base project.  Base project branch will keep the very early stages of development.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
+import { MaterialModule } from './material.module';
+import { AppComponent } from './app.component';
+
+// Core imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Firebase imports
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore'; // Firestore database
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularFireModule,
+    AngularFirestoreModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
